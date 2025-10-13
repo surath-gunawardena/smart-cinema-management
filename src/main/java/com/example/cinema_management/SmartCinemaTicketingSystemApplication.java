5979a2b606aebe0fd0903c535d87d74c3ea5ef6c
suangi-admin <susangi@mylinex.com>
2025-10-13 21:15:44 +0530

    init project

---
+++ src/main/java/com/example/cinema_management/SmartCinemaTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCinemaTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCinemaTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cinema_management/SmartCinemaTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinema_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCinemaTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
