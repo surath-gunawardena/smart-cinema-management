1bb8e0432b4b949bbe34c6f7e12de42d575ea5bb
Surath Gunawardena <surath.gunawardena@layoutindex.lk>
2025-10-21 20:40:41 +0530

    Movie Module fine tune and finalized

--- src/main/java/com/example/cinema_management/movie/dto/MovieResponse.java
+++ src/main/java/com/example/cinema_management/movie/dto/MovieResponse.java
@@ -1,18 +1,17 @@
 package com.example.cinema_management.movie.dto;
 
-
 import java.time.LocalDate;
-
 
 public record MovieResponse(
         Long id,
         String title,
         String description,
-        String genre,
-        String language,
         Integer durationMinutes,
+        String language,
+        String genre,
+        Double rating,
         LocalDate releaseDate,
-        String rating,
-        String posterUrl,
+        String posterPath,
+        String posterUrl,     // ✅ add this
         boolean active
 ) {}
